489c55c025df693d4ca143b0a096819f7ec95f8b
Dmitriy E <dimayef@gmail.com>
2018-05-13 21:06:39 +0300

    initial commit Goods catalog

--- wp-content/plugins/goods/class.goods-taxonomy.php
+++ wp-content/plugins/goods/class.goods-taxonomy.php
@@ -23,20 +23,19 @@ class Goods_Taxonomy {
 				'new_item_name'     => 'Название категории',
 				'menu_name'         => 'Таксономии',
 			),
-			'description'           => 'Категории', // описание таксономии
+			'description'           => 'Категории',
 			'public'                => true,
-			'publicly_queryable'    => null, // равен аргументу public
-			'show_in_nav_menus'     => true, // равен аргументу public
-			'show_ui'               => true, // равен аргументу public
-			'show_in_menu'          => true, // равен аргументу show_ui
+			'publicly_queryable'    => null,
+			'show_in_nav_menus'     => true,
+			'show_ui'               => true, 
+			'show_in_menu'          => true,
 			'hierarchical'          => true,
 			'update_count_callback' => '',
 			'rewrite'               => true,
 			'capabilities'          => array(),
-			'meta_box_cb'           => null, // callback функция. Отвечает за html код метабокса (с версии 3.8): post_categories_meta_box или post_tags_meta_box. Если указать false, то метабокс будет отключен вообще
-			'show_admin_column'     => false, // Позволить или нет авто-создание колонки таксономии в таблице ассоциированного типа записи. (с версии 3.5)
+			'show_admin_column'     => false,
 			'_builtin'              => false,
-			'show_in_quick_edit'    => null, // по умолчанию значение show_ui
+			'show_in_quick_edit'    => null,
 		) );
 	}
 
